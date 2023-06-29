e3c41465d3ef760b19932560d4143be425f5020e
Dylan <dylankane3@gmail.com>
2023-06-29 11:55:21 +0000

    Add choices field to ForumPost model

--- forum/models.py
+++ forum/models.py
@@ -6,6 +6,17 @@ from cloudinary.models import CloudinaryField
 
 
 class ForumPost(models.Model):
+
+    class Bikes(models.TextChoices):
+        NONE = 'N/A', 'N/A'
+        ROAD = 'RD', 'Road'
+        MTB = 'MTB', 'Mtb'
+        GRAVEL = 'GRVL', 'Gravel'
+        FIXIE = 'FIX', 'Fixie'
+        TOURING = 'TR', 'Touring'
+        BMX = 'BMX', 'Bmx'
+        HYBRID = 'HYB', 'Hybrid'
+
     title = models.CharField(max_length=150, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_posts")
     slug = models.SlugField(max_length=150, unique=True)
@@ -13,6 +24,7 @@ class ForumPost(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     cover_image = CloudinaryField('image', default="placeholder")
     content = models.TextField()
+    bikes = models.CharField(max_length=20, choices=Bikes.choices, default=Bikes.NONE)
 
     class Meta:
         ordering = ['created_on']

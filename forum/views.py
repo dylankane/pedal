bcbf715d19eae07ba3551592940f2291a5d0bcee
Dylan <dylankane3@gmail.com>
2023-07-02 17:41:52 +0000

    Render the posts to the index.html template

--- forum/views.py
+++ forum/views.py
@@ -3,9 +3,9 @@ from django.views import generic
 from .models import ForumPost
 
 
-# Create your views here.
 class PostList(generic.ListView):
     model = ForumPost
     queryset = ForumPost.objects.order_by('-created_on')
     template_name = 'index.html'
+    context_object_name = "posts"
     paginate_by = 9

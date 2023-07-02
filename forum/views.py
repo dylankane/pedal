0694c9baa4a8b490686eda8322b0935bf7e6e70f
Dylan <dylankane3@gmail.com>
2023-07-02 20:06:06 +0000

    Add a class in views.py to render individual posts

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import ForumPost
 
 
@@ -9,3 +9,21 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     context_object_name = "posts"
     paginate_by = 9
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = ForumPost.objects.all()
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comment.order_by("created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request, {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )

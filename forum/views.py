6e6d20c41738e811f196886b075bee6e506ed283
Dylan <dylankane3@gmail.com>
2023-07-04 11:39:08 +0000

    Create a post method for handling the comment form

--- forum/views.py
+++ forum/views.py
@@ -32,3 +32,34 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = ForumPost.objects.all()
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.all().order_by("created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            # comment_form.instance.email = request.user.email
+            comment_form.instance.author = request.user
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+        else:
+            comment_form = CommentForm
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )

3c11d3cfef84cc006495b1a88c6403f38421a2f6
Dylan <dylankane3@gmail.com>
2023-07-06 20:40:26 +0000

    Create url in urls.py for Profile view

--- forum/urls.py
+++ forum/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('create_post/', views.CreatePost.as_view(), name='create_post'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('profile/', views.Profile.as_view(), name='profile'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]

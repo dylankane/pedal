6e8cdffbb61d4c7240ccca16e71e266295716410
Dylan <dylankane3@gmail.com>
2023-07-07 13:15:26 +0000

    Format code and add description comments to views

--- forum/views.py
+++ forum/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 
 
+# Class to show all posts on the homepage as list, targets the ForumPost model,
+# gets all posts and orders them by creation date,
+# paginates page by 9 posts, and renders to template.
 class PostList(generic.ListView):
     model = ForumPost
     queryset = ForumPost.objects.order_by('-created_on')
@@ -16,8 +19,14 @@ class PostList(generic.ListView):
     paginate_by = 9
 
 
+# Class that handles the view of each individual post,
+# with like and commenting functionality
 class PostDetail(View):
 
+    # Updating the get method for generic View, creates object of all posts,
+    # selects the one being viewed by the slug field, also gets all comments
+    # on this post, and checks if user has liked this post. Then renders all
+    # info to the template.
     def get(self, request, slug, *args, **kwargs):
         queryset = ForumPost.objects.all()
         post = get_object_or_404(queryset, slug=slug)
@@ -37,6 +46,11 @@ class PostDetail(View):
             },
         )
 
+    # Updates the post method on the generic view, again collecting a set of
+    # the posts and selecting by one by slug, Then refering to the form
+    # CommentForm from forms.py, If this form is valid it triggers succes
+    # message, sets author as the current user name, and saves to the Comment
+    # database
     def post(self, request, slug, *args, **kwargs):
         queryset = ForumPost.objects.all()
         post = get_object_or_404(queryset, slug=slug)
@@ -70,6 +84,10 @@ class PostDetail(View):
         )
 
 
+# Class to handle the like functionality of the posts,
+# retrievs the object/post by its slug field, and checks if user has liked or
+#  not, with logic to add or remove the like field, and redirecting to the
+#   currrent page / post view.
 class PostLike(View):
     def post(self, request, slug):
         post = get_object_or_404(ForumPost, slug=slug)
@@ -82,17 +100,41 @@ class PostLike(View):
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
+# Class to create a view of the posts creted by the current user,
+# Targets the ForumPost model, sets a template to render to, and paginates by 9.
+class Profile(generic.ListView):
+    model = ForumPost
+    template_name = 'profile.html'
+    context_object_name = "posts"
+    paginate_by = 9
+
+    # Sets user to the current logged in user, and retrieves a set of all posts
+    # with the author field matching that name
+    def get_queryset(self):
+        user = self.request.user
+        queryset = ForumPost.objects.filter(author=user)
+        return queryset
+
+
+# Class to handle user creating a post, declares the model its working on,
+# the template to render to and the form from forms.py
 class CreatePost(generic.CreateView):
     model = ForumPost
     template_name = 'create_post.html'
     form_class = PostForm
 
+    # Updating / overiding the formvalid method from generic CreateView
+    # When form is valid, trigger a success message and sets the author name to
+    # that of the current user.
     def form_valid(self, form):
         messages.success(
             self.request, "Your bike has been successfully posted")
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+    # Updates the succuss url method of the generic CreateView.
+    # Retrieving the slug of the newly created post and using it to set the
+    # url, to redirect user to the post detail view of that newly created post
     def get_success_url(self):
         slug = self.object.slug
         success_url = reverse('post_detail', kwargs={'slug': slug})

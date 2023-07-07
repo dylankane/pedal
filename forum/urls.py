f875d062c87d65d4fdbd299f75af84ad0f5697a7
Dylan <dylankane3@gmail.com>
2023-07-07 21:39:54 +0000

    Create template and url for categories view

--- forum/urls.py
+++ forum/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('categories/', views.Categories.as_view(), name='categories'),
     path('create_post/', views.CreatePost.as_view(), name='create_post'),
     path('profile/', views.Profile.as_view(), name='profile'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),

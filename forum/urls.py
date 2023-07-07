6a9d6943f7420ab18db363164844287d1df891bd
Dylan <dylankane3@gmail.com>
2023-07-07 13:12:59 +0000

    Create urls for update and delete views

--- forum/urls.py
+++ forum/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('profile/', views.Profile.as_view(), name='profile'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/update_post', views.UpdatePost.as_view(), name='update_post'),
+    path('<slug:slug>/delete_post', views.DeletePost.as_view(), name='delete_post'),
 ]

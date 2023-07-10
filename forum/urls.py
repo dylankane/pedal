00154d6ef2c8e5a72ecec76066791fc2f13bb589
Dylan <dylankane3@gmail.com>
2023-07-10 17:43:52 +0000

    Change post_detail url to include a primary key

--- forum/urls.py
+++ forum/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('categories/<str:cats>/', views.Categories, name='categories'),
-    path('create_post/', views.CreatePost.as_view(), name='create_post'),
     path('profile/', views.Profile.as_view(), name='profile'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('<int:pk>/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('categories/<str:cats>/', views.categories, name='categories'),
+    path('create_post/', views.CreatePost.as_view(), name='create_post'),
+    path('like/<int:pk><slug:slug>', views.PostLike.as_view(), name='post_like'),
     path(
         '<slug:slug>/update_post',
         views.UpdatePost.as_view(),

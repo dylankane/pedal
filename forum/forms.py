0e755ad9c5b5876464917fc648dc5c337c997ca3
Dylan <dylankane3@gmail.com>
2023-07-28 07:27:10 +0000

    Add code comments to forms.py

--- forum/forms.py
+++ forum/forms.py
@@ -2,18 +2,21 @@ from .models import Comment, ForumPost
 from django import forms
 
 
+# Form for creating a comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
 
 
+#Form for user to edit their own comments 
 class EditCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
 
 
+#Form for a logged-in user to create their own post
 class PostForm(forms.ModelForm):
     class Meta:
         model = ForumPost
@@ -29,6 +32,7 @@ class PostForm(forms.ModelForm):
             )
 
 
+#Form for a user to update their own posts.
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = ForumPost
